eb513062c040a1a72a7b9e122b49b6623188b9a2
Howard Cheng <howard.cheng@oneplus.com>
2020-05-02 17:23:58 +0800

    remove redundant code

--- src/main/java/utils/DateUtil.java
+++ src/main/java/utils/DateUtil.java
@@ -1,6 +1,5 @@
 package utils;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.HashMap;
@@ -10,11 +9,7 @@ public class DateUtil {
         HashMap<YearMonth, Long> map = new HashMap<>();
         for (LocalDate date = start; date.isBefore(end.plusDays(1)); date = date.plusDays(1)) {
             YearMonth key = YearMonth.of(date.getYear(), date.getMonthValue());
-            if (!map.containsKey(key)) {
-                map.put(key, 0L);
-            }
-
-            long current = map.get(key);
+            long current = map.getOrDefault(key, 0L);
             map.put(key, current + 1);
         }
 
