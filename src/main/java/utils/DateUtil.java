a2d98fe7e5b4bc48cf8ed5cb716e9f84f2a5b1cc
Howard Cheng <howard.cheng@oneplus.com>
2020-05-02 17:05:07 +0800

    refactor dateutils

--- src/main/java/utils/DateUtil.java
+++ src/main/java/utils/DateUtil.java
@@ -8,34 +8,14 @@ import java.util.HashMap;
 public class DateUtil {
     public static HashMap<YearMonth, Long> getDateList(LocalDate start, LocalDate end) {
         HashMap<YearMonth, Long> map = new HashMap<>();
-        if (start.getYear() == end.getYear()) {
-            if (start.getMonthValue() == end.getMonthValue()) {
-                map.put(YearMonth.of(start.getYear(), start.getMonthValue()),
-                        Duration.between(start, end).toDays());
-            } else {
-                for (int m = start.getMonthValue(); m <= end.getMonthValue(); m++) {
-                    if (m == end.getMonthValue()) {
-                        map.put(YearMonth.of(start.getYear(), m), Duration.between(start, end).toDays());
-                    } else {
-                        map.put(YearMonth.of(start.getYear(), m), (long) (start.lengthOfMonth() - start.getDayOfMonth() + 1));
-                    }
-                }
+        for (LocalDate date = start; date.isBefore(end.plusDays(1)); date = date.plusDays(1)) {
+            YearMonth key = YearMonth.of(date.getYear(), date.getMonthValue());
+            if (!map.containsKey(key)) {
+                map.put(key, 0L);
             }
-        } else {
-            for (int y = start.getYear(); y <= end.getYear(); y++) {
-                if (start.getMonthValue() == end.getMonthValue()) {
-                    map.put(YearMonth.of(start.getYear(), start.getMonthValue()),
-                            Duration.between(start, end).toDays());
-                } else {
-                    for (int m = start.getMonthValue(); m <= end.getMonthValue(); m++) {
-                        if (m == end.getMonthValue()) {
-                            map.put(YearMonth.of(start.getYear(), m), Duration.between(start, end).toDays());
-                        } else {
-                            map.put(YearMonth.of(start.getYear(), m), (long) (start.lengthOfMonth() - start.getDayOfMonth() + 1));
-                        }
-                    }
-                }
-            }
+
+            long current = map.get(key);
+            map.put(key, current + 1);
         }
 
         return map;
