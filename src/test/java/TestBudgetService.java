6fca249d5e5aba0fb1eb917bb67c09ba8ca28e78
James Yeh <yehjames23@gmail.com>
2020-05-02 16:05:32 +0800

    add budget class

--- src/test/java/TestBudgetService.java
+++ src/test/java/TestBudgetService.java
@@ -1,16 +1,56 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner::class)
 public class TestBudgetService {
 
 
+    @Mock private IBudgetRepo budgetRepo;
+
+    @Test
+    public void sameDay(){
+        BudgetService budgetService = new BudgetService();
+
+        when(budgetRepo.getAll()).thenReturn(Arrays.asList(new Budget()));
+
+        budgetService.query(LocalDate.of(2020, 04, 01),
+                            LocalDate.of(2020, 04, 01));
+    }
 
     @Test
     public void sameMonth(){
         BudgetService budgetService = new BudgetService();
-        budgetService.query(LocalDatete.of(2020, 04, 01));
+        budgetService.query(LocalDate.of(2020, 04, 01),
+                            LocalDate.of(2020, 04, 30));
     }
+
+    @Test
+    public void crossTwoMonth(){
+        BudgetService budgetService = new BudgetService();
+        budgetService.query(LocalDate.of(2020, 04, 01),
+                            LocalDate.of(2020, 05, 15));
+    }
+
+    @Test
+    public void crossThreeMonth(){
+        BudgetService budgetService = new BudgetService();
+        budgetService.query(LocalDate.of(2020, 04, 01),
+                            LocalDate.of(2020, 06, 15));
+    }
+
+    @Test
+    public void startDayGrtEndDay(){
+        BudgetService budgetService = new BudgetService();
+        budgetService.query(LocalDate.of(2020, 04, 01),
+                LocalDate.of(2020, 02, 15));
+    }
+
+
 }
